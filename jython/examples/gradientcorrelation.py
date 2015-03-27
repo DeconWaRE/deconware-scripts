e2fed87570aa00b13b90b68265cc6173e4a66bc1
Brian Northan <bnorthan@gmail.com>
2015-03-27 17:17:04 -0400

    make images a parameter:

--- jython/examples/gradientcorrelation.py
+++ jython/examples/gradientcorrelation.py
@@ -2,27 +2,14 @@
 # @DisplayService display
 # @IOService io
 # @OpService ops
+# @net.imagej.Dataset image
+# @net.imagej.Dataset template
 
 from net.imglib2.img.display.imagej import ImageJFunctions;
 from net.imglib2.type.numeric.real import FloatType;
 from net.imglib2.type.numeric.complex import ComplexFloatType;
 
 from jarray import array
-
-# define a local directory to get the images from
-# TODO:  put images in same directory as the script then get the script directory
-directory="/home/bnorthan//Brian2012/Round2/deconware2/imglib2-tutorials/"
-
-imageName="DrosophilaWing.tif"
-templateName="WingTemplate.tif"
-
-# open and display the image
-image=data.open(directory+imageName)
-display.createDisplay(image.getName(), image);	
-
-# open and display the template
-template=data.open(directory+templateName)
-display.createDisplay(template.getName(), template);	
 
 # create a log kernel
 logKernel=ops.logKernel(image.numDimensions(), 1.0);
