a36e43b708d272f243425bad3389a04f627a5819
Brian Northan <bnorthan@gmail.com>
2015-03-27 17:12:12 -0400

    pass images as parameters

--- jython/deconvolve/ConvolveDeconvolveOpsTest.py
+++ jython/deconvolve/ConvolveDeconvolveOpsTest.py
@@ -2,47 +2,32 @@
 # @DisplayService display
 # @IOService io
 # @OpService ops
+# @net.imagej.Dataset image
+# @net.imagej.Dataset psf
 
 from net.imglib2.meta import ImgPlus
 from net.imglib2.type.numeric.real import FloatType
 
 from jarray import array
 
-# define a local directory to get the images from
-directory="/home/bnorthan/Brian2012/Round2/deconware2/deconware-scripts/images/"
+sumImg=ops.sum(FloatType(), image.getImgPlus())
+sumPsf=ops.sum(FloatType(), psf.getImgPlus())
 
-imageName="Bars72.tif"
-psfName="BornWolf72.tif"
-
-# open the image
-image=data.open(directory+imageName)
-display.createDisplay(image.getName(), image);	
-
-thesum=ops.sum(FloatType(), image)
-print thesum
-
-# open the psf
-psf=data.open(directory+psfName)
-display.createDisplay(psf.getName(), psf);
-
-psfsum=ops.sum(FloatType(), psf)
-print psfsum
-
-convolved=ops.convolve(image, psf);
+convolved=ops.convolve(image.getImgPlus(), psf.getImgPlus());
 display.createDisplay("convolved", ImgPlus(convolved));
-
-outsum=ops.sum(FloatType(), convolved)
-print outsum
 
 size=array([0, 0, 0], 'l')
 convolved2=ops.convolve(None, image, psf, size)
 display.createDisplay("convolved2", ImgPlus(convolved2));
 
-outsum2=ops.sum(FloatType(), convolved2)
-print outsum2
-print thesum.getRealDouble()
-print psfsum.getRealDouble()
-print thesum.getRealDouble()*psfsum.getRealDouble()
+sumConvolved=ops.sum(FloatType(), convolved)
+sumConvolved2=ops.sum(FloatType(), convolved2)
+
+print sumImg.getRealDouble()
+print sumPsf.getRealDouble()
+print sumImg.getRealDouble()*sumPsf.getRealDouble()
+print sumConvolved.getRealDouble()
+print sumConvolved2.getRealDouble()
 
 deconvolved=ops.run("deconvolve", convolved, psf, 10);
 display.createDisplay("deconvolved", ImgPlus(deconvolved));
