2e7f2461b8e162b5243a7fe89b9f3b0a8f4c8706
Brian Northan <bnorthan@gmail.com>
2015-03-27 17:13:40 -0400

    make 2D phantom

--- jython/deconvolve/MakePhantom.py
+++ jython/deconvolve/MakePhantom.py
@@ -6,36 +6,26 @@
 # empty image
 
 from jarray import zeros
+from jarray import array
 from net.imglib2.type.numeric.real import FloatType
 from net.imglib2.meta import ImgPlus
 
-size=zeros(3,'i')
-size[0]=128
-size[1]=128
-size[2]=64
+size=array([144, 144], 'i')
+image=ops.createimg(size)
 
-image=ops.run("create", size,  FloatType())
-
-imgPlus=ImgPlus(image, "phantom")
-
-display.createDisplay("phantom",  data.create(imgPlus));
-
-location=zeros(3,'i')
-location[0]=size[0]/2;
-location[1]=size[1]/2;
-location[2]=size[2]/2;
+location=array([size[0]/2, size[1]/2], 'i')
 
 radius1=20
 radius2=17
 
-aradius=zeros(3,'l')
-aradius[0]=10
-aradius[1]=10
-aradius[2]=30
+aradius=array([20, 20], 'i')
 
 #ops.run("addsphere",  image, location, radius, 1.0)
 #ops.run("addassymetricspherel",  image, location, 1.0, radius1, radius2)
 ops.run("addassymetricsphere",  image, location, 1.0, aradius)
+
+display.createDisplay("phantom",  image);
+
 
 
 
