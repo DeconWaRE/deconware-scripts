7d49bfa4c52aff94e6c1642b360a1acea4f7db19
Brian Northan <bnorthan@gmail.com>
2015-03-27 17:14:07 -0400

    Add DeconvolveOpsTest

---
+++ jython/deconvolve/DeconvolveOpsTest.py
@@ -0,0 +1,17 @@
+# @DisplayService display
+# @OpService ops
+# @net.imagej.Dataset image
+# @net.imagej.Dataset psf
+
+from net.imglib2.meta import ImgPlus
+from jarray import array
+
+
+# by default when we deconvolve the image is extended to avoid wrap around during convolution
+#deconvolved=ops.run("deconvolve", image, psf, 10);
+
+# alternatively we can define an extension size.  Here we set the extension size to be 0 in all dimensions
+# this means the image won't be extended (other then extension that is done internally to reach a fast fft size)
+size=array([0, 0, 0], 'l')
+deconvolved=ops.run("deconvolve", None, image, psf, size, 10);
+display.createDisplay("deconvolved", ImgPlus(deconvolved));
