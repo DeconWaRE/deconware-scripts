4ca49733aaa238e34463ab31541dd4c74f44c2b3
root <bnorthan@gmail.com>
2015-07-08 08:39:18 -0400

    update phantom script

--- jython/deconvolve/MakePhantom.py
+++ jython/deconvolve/MakePhantom.py
@@ -3,7 +3,6 @@
 # @IOService io
 # @OpService ops
 # @net.imagej.Dataset psf
-# @net.imagej.Dataset image
 
 from ij import IJ
 
@@ -15,18 +14,15 @@ from net.imglib2.img.display.imagej import ImageJFunctions
 
 from com.deconware.algorithms.noise import AddPoissonNoise
 
-size=array([256, 256, 256], 'i')
+size=[256,256,256]
 image=ops.createimg(size)
 
-location=array([size[0]/2, size[1]/2, size[2]/2+50], 'i')
-
+location=[size[0]/2, size[1]/2, size[2]/2+50]
 radius1=48
 radius2=44
 
 background=0.0
 foreground=200.0
-
-aradius=array([20, 20], 'i')
 
 #ops.run("addsphere",  image, location, radius, 1.0)
 #ops.run("addassymetricspherel",  image, location, 1.0, radius1, radius2)
@@ -34,24 +30,27 @@ aradius=array([20, 20], 'i')
 ops.run("addconstant", image, background)
 ops.run("addshell", image, location, foreground, background, radius1, radius2)
 
-convolved=ops.convolve(image, psf);
+# show phantom
 display.createDisplay("phantom",  image);
+
+# crop phantom (replace with crop op)
 impPhantom=IJ.getImage()
 impPhantom.setRoi(56, 56,144,144);
-
 IJ.run(impPhantom, "Crop", "");
 IJ.run(impPhantom, "Make Substack...", "frames=56-199");
 
-noiser=AddPoissonNoise(convolved)
-#noiser.process()
+# convolve
+convolved=ops.convolve(image, psf);
 
+# add poisson noise
+noiser=AddPoissonNoise(convolved)
+noiser.process()
+
+# show convolved
 display.createDisplay("convolved",  ImgPlus(convolved));
 
-# convert to imagej1 imageplus so we can run analyze particles
-# impConvolved=ImageJFunctions.wrap(convolved, "wrapped")
+# crop convolved (replace with crop op)
 impConvolved=IJ.getImage()
 impConvolved.setRoi(56, 56,144,144);
-
 IJ.run(impConvolved, "Crop", "");
 IJ.run(impConvolved, "Make Substack...", "frames=56-199");
-
